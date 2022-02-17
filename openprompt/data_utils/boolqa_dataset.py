798ef75dffaa9c0657a8acf5325147663ee462c4
Cookize <zy.song@qq.com>
2022-02-17 13:48:03 +0800

    fix "IFLYTEKBoolQA" dataset

--- openprompt/data_utils/boolqa_dataset.py
+++ openprompt/data_utils/boolqa_dataset.py
@@ -107,13 +107,14 @@ class IFLYTEKBoolQADataset(DataProcessor):
         super().__init__(["否", "是"])
         self.CONTEXT_LENGTH = [50, 800]
         self.id2topic = None
-        
-    def get_examples(self, data_dir: Optional[str] = None, split: Optional[str] = None) -> List[InputExample]:
 
+    
+    def get_examples(self, data_dir: Optional[str] = None, split: Optional[str] = None) -> List[InputExample]:
+        
         # load topics
         if self.id2topic == None:
             self.load_topics(data_dir)
-
+        
         examples = []
         raw_data = []
 
@@ -122,6 +123,19 @@ class IFLYTEKBoolQADataset(DataProcessor):
             decoder = json.JSONDecoder()
             for line in load_f:
                 raw_data.append(decoder.decode(line))
+
+        if split == "train":
+            examples = self.get_lite_examples(raw_data=raw_data, split=split, contrast=True)
+        elif split == "dev":
+            examples = self.get_lite_examples(raw_data=raw_data, split=split, contrast=True)
+        elif split == "test":
+            examples = self.get_lite_examples(raw_data=raw_data, split=split, contrast=True)
+
+        return examples
+        
+    def get_lite_examples(self, raw_data: Optional[list] = None, split: Optional[str] = None, contrast: bool = False) -> List[InputExample]:
+
+        examples = []
 
         count_yes = 0
         count_no = 0
@@ -148,11 +162,6 @@ class IFLYTEKBoolQADataset(DataProcessor):
                 count_to_short += 1
                 continue
 
-            incorrect_topic = random.randint(0, 117)
-            if incorrect_topic >= correct_topic:
-                incorrect_topic += 1
-            incorrect_topic_text = self.id2topic[incorrect_topic]
-
             positive_data = InputExample(
                 guid=str(count_global),
                 text_a=context,
@@ -164,21 +173,140 @@ class IFLYTEKBoolQADataset(DataProcessor):
             count_global += 1
             count_yes += 1
 
-            negative_data = InputExample(
-                guid=str(count_global),
-                text_a=context,
-                text_b=incorrect_topic_text,
-                tgt_text=self.labels[0],
-                label=0,
-            )
-            examples.append(negative_data)
-            count_global += 1
-            count_no += 1
+            if contrast:
+                incorrect_topic = random.randint(0, 117)
+                if incorrect_topic >= correct_topic:
+                    incorrect_topic += 1
+                incorrect_topic_text = self.id2topic[incorrect_topic]
+
+                negative_data = InputExample(
+                    guid=str(count_global),
+                    text_a=context,
+                    text_b=incorrect_topic_text,
+                    tgt_text=self.labels[0],
+                    label=0,
+                )
+                examples.append(negative_data)
+                count_global += 1
+                count_no += 1
 
         print("Total:%d, Yes:%d, No:%d" % (count_global, count_yes, count_no))
         print("Deleted:\n\tTo_long:%d, To_short:%d" % (count_to_long, count_to_short))
 
         print(examples[0].text_a)
+        print(examples[0].text_b)
+
+        return examples
+
+    def get_full_examples(self, raw_data: Optional[list] = None, split: Optional[str] = None) -> List[InputExample]:
+
+        examples = []
+
+        count_yes = 0
+        count_no = 0
+        count_global = 0
+        count_to_long = 0
+        count_to_short = 0
+
+        for i in tqdm(range(len(raw_data)), desc="Processing {} data of IFLYTEKBoolQA".format(split)):
+
+            sample = raw_data[i]
+
+            correct_topic = int(sample["label"], base=10)
+
+            # delete the topic 'others'
+            if correct_topic == 118:
+                continue
+
+            context = "".join(sample["sentence"]).replace(" ", "").replace("，", ",").replace("。", ".")
+            if len(context) > self.CONTEXT_LENGTH[1]:
+                count_to_long += 1
+                continue
+            elif len(context) < self.CONTEXT_LENGTH[0]:
+                count_to_short += 1
+                continue
+
+            for topic, topic_text in self.id2topic.items():
+
+                topic_text = self.id2topic[topic]
+                label = 0
+
+                if topic == correct_topic:
+                    label = 1
+                    count_yes += 1
+                else:
+                    count_no +=1
+
+                new_example = InputExample(
+                    guid=str(count_global),
+                    text_a=context,
+                    text_b=topic_text,
+                    tgt_text=self.labels[label],
+                    label=label,
+                )
+                examples.append(new_example)
+                count_global += 1
+
+        print("Total:%d, Yes:%d, No:%d" % (count_global, count_yes, count_no))
+        print("Deleted:\n\tTo_long:%d, To_short:%d" % (count_to_long, count_to_short))
+
+        print(examples[0].text_a)
+        print(examples[0].text_b)
+
+        return examples
+
+    def get_random_example(self, raw_data: Optional[list] = None, split: Optional[str] = None) -> List[InputExample]:
+        
+        examples = []
+
+        count_yes = 0
+        count_no = 0
+        count_global = 0
+        count_to_long = 0
+        count_to_short = 0
+
+        for i in tqdm(range(len(raw_data)), desc="Processing {} data of IFLYTEKBoolQA".format(split)):
+
+            sample = raw_data[i]
+
+            correct_topic = int(sample["label"], base=10)
+            label = 0
+
+            # delete the topic 'others'
+            if correct_topic == 118:
+                continue
+
+            context = "".join(sample["sentence"]).replace(" ", "").replace("，", ",").replace("。", ".")
+            if len(context) > self.CONTEXT_LENGTH[1]:
+                count_to_long += 1
+                continue
+            elif len(context) < self.CONTEXT_LENGTH[0]:
+                count_to_short += 1
+                continue
+
+            random_topic = random.randint(0, 118)
+            if random_topic == correct_topic:
+                label == 1
+                count_yes += 1
+            else:
+                count_no += 1
+            topic_text = self.id2topic[random_topic]
+
+            random_data = InputExample(
+                guid=str(count_global),
+                text_a=context,
+                text_b=topic_text,
+                tgt_text=self.labels[label],
+                label=label,
+            )
+            examples.append(random_data)
+            count_global += 1
+
+        print("Total:%d, Yes:%d, No:%d" % (count_global, count_yes, count_no))
+        print("Deleted:\n\tTo_long:%d, To_short:%d" % (count_to_long, count_to_short))
+
+        print(examples[0].text_a)
+        print(examples[0].text_b)
 
         return examples
 
